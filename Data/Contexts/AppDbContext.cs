7047cc8ca6f852922c940fd29bf97d0a3a865d24
yodemisj <yodemisj@gmail.com>
2024-09-07 14:17:09 -0300

    feat: configure AppDbContext

--- Data/Contexts/AppDbContext.cs
+++ Data/Contexts/AppDbContext.cs
@@ -3,5 +3,14 @@ namespace neighborhoodFlavors.Data;
 
 public class AppDbContext: DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {
+
+    }
     public DbSet<Comment> Comment { get; set; }
+    public DbSet<Item> Item { get; set; }
+    public DbSet<Menu> Menu { get; set; }
+    public DbSet<Rating> Rating { get; set; }
+    public DbSet<Restaurant> Restaurant { get; set; }
+    public DbSet<User> User { get; set; }
+
 }
