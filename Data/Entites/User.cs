190e26316f068bc2896b0c86a7d0ec993699c456
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-09-11 20:57:26 -0300

    fix: references is not null on Home.razor file

--- Data/Entites/User.cs
+++ Data/Entites/User.cs
@@ -5,5 +5,5 @@ public class User
     public int Id { get; set; }
     public String? Name { get; set; }
     public String? Email { get; set; }
-    public String? Password { get; set; }
+    public String ? Password { get; set; }
 }
